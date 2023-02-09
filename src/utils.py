df8f9ad0cad99ec1a494323bedefe47ff4cb5a1d
Ryan Krueger <ryankrue@umich.edu>
2023-02-09 13:50:28 -0500

    Reorg

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,10 @@
 import pdb
 
-import jax
 import jax.numpy as jnp
 from jax import random
 from jax import vmap
 from jax.config import config as jax_config
 
-from jax_md import quantity
-from jax_md import simulate
-from jax_md import space
-from jax_md import energy
-from jax_md import test_util
-from jax_md import partition
 from jax_md import util
 from jax_md import rigid_body
 
@@ -58,39 +51,5 @@ def get_rand_rigid_body(n, box_size, key):
     body = rigid_body.RigidBody(R, quaternion)
     return body
 
-def gen_tables(mu, nu, pairwise_energies_fn):
-    eps_mu_mu = pairwise_energies_fn(mu, mu)
-    eps_mu_nu = pairwise_energies_fn(mu, nu)
-    eps_nu_mu = pairwise_energies_fn(nu, mu)
-
-    return eps_mu_mu, eps_mu_nu, eps_nu_mu
-
-
-# p1 and p2 are rigid bodies of size 1
-def pairwise_energy_fn(p1, p2):
-    dist = jnp.linalg.norm(p1.center - p2.center)
-    # return center_energy_fn(dist)
-    # return energy.soft_sphere(dist)
-    # return energy.simple_spring(dist)
-    # return energy.lennard_jones(dist)
-    return energy.simple_spring(dist, length=0.5) + energy.soft_sphere(dist)
-get_pairwise_energies = vmap(vmap(pairwise_energy_fn, in_axes=(None, 0)),
-                                 in_axes=(0, None))
-
-
 if __name__ == "__main__":
-    import vmmc
-
-    n = 20
-    # box_size = quantity.box_size_at_number_density(n, 0.1, 3)
-    box_size = 20
-    displacement, shift = space.periodic(box_size)
-
-    key = random.PRNGKey(0)
-
-    key, body_key = random.split(key, 2)
-    init_body = get_rand_rigid_body(n, box_size, body_key)
-
-    fin_state, traj = vmmc.vmmc(init_body,
-                                partial(gen_tables, pairwise_energies_fn=get_pairwise_energies),
-                                key)
+    pass
---
+++ src/vmmc.py
@@ -0,0 +1,151 @@
+import pdb
+from functools import partial
+from tqdm import tqdm
+
+import jax
+import jax.numpy as jnp
+from jax import random
+from jax import vmap
+from jax.config import config as jax_config
+
+from jax_md import rigid_body
+
+import utils
+
+
+"""
+Strategy:
+
+Notation:
+- A is prelink table
+- B is reverse link table (note: we compute separately for now, but could be A.T)
+- A * (1 - B) is frustrated
+- not A is failed -- note: does not include not B
+- A and B is link formed -- note: includes the `min` step
+
+Procedure for precomputing table of links (`all_link_coinflips`):
+- compute A, including its coinflips. `prelink_coinflips`
+- compute failed matrix, F, as (not A_coinflip)
+- compute B as min(1, A / A.T) element-wise (the `min` expression)
+- compute `B_coinflip`
+- compute C: A_coinflip AND B_coinflip -- this is what you use in the floodfill
+- update C: set diagonal to be 1
+
+Procedure for building cluster given table of links -- flood fill:
+- given adjacency matrix A: nxn
+- randomly sample a seed vertex
+- get the corresponding row in the precomputed, one-hot link matrix
+  - x: (n,)
+- repeat x*A: (n,) n times
+  - scan over x
+  - final x will just be things in the cluster.
+
+Note:
+- assumes that order doesn't matter
+  - i.e. link formation of ij doesn't depend on link formation of kl
+"""
+def vmmc(body, gen_tables_fn, key, n_steps=10, temp=0.3):
+    n = body.center.shape[0]
+    seed_vertices_key, key = random.split(key, 2)
+    seed_vertices = jax.random.randint(seed_vertices_key, (n_steps,), minval=0, maxval=n-1)
+
+    beta = 1/temp
+    def boltz(x):
+        return jnp.exp(-beta * x)
+    iter_keys = random.split(key, n_steps)
+
+    mu = body
+    traj = [mu]
+
+    for i, iter_key, seed_vertex in tqdm(zip(range(n_steps), iter_keys, seed_vertices)):
+        iter_key, displacement_key = random.split(iter_key, 2)
+        move = utils.gen_random_displacement(r_min=0.5, r_max=1.0, key=displacement_key)
+        nu = rigid_body.RigidBody(mu.center + move, mu.orientation)
+
+        eps_mu_mu, eps_mu_nu, eps_nu_mu = gen_tables_fn(mu, nu)
+
+
+        # Precompute the coinflips
+        prelink_diffs = eps_nu_mu - eps_mu_mu # eps_ip_j^mu - eps_i_j^mu
+        prelink_boltz = boltz(prelink_diffs)
+        prelink_probs = jnp.maximum(0, 1-prelink_boltz) # can be made branchless
+        iter_key, prelink_key = random.split(iter_key, 2)
+        prelink_coinflip_thresholds = random.uniform(prelink_key, shape=prelink_probs.shape)
+        prelink_coinflips = (prelink_probs > prelink_coinflip_thresholds).astype(jnp.int32) # matrix A
+
+        rev_link_diffs = eps_mu_nu - eps_mu_mu # eps_i_jp^mu - eps_i_j^mu
+        rev_link_boltz = boltz(rev_link_diffs)
+        rev_link_probs = jnp.maximum(0, 1-rev_link_boltz) # can be made branchless
+        ratio = jnp.where(prelink_probs == 0, 0.0, rev_link_probs / prelink_probs)
+        all_link_probs = jnp.minimum(ratio, 1.0) # can be made branchless
+        iter_key, all_link_key = random.split(iter_key, 2)
+        all_link_coinflip_thresholds = random.uniform(all_link_key, shape=all_link_probs.shape)
+        arr_B = (all_link_probs > all_link_coinflip_thresholds).astype(jnp.int32) # matrix B
+
+        all_link_coinflips = jnp.multiply(prelink_coinflips, arr_B)
+        all_link_coinflips = all_link_coinflips + jnp.eye(all_link_coinflips.shape[0]) # matrix C
+
+        frustrated = jnp.multiply(prelink_coinflips, 1 - arr_B) # frustrated
+
+
+        # Do the floodfill
+        seed_row = all_link_coinflips[seed_vertex]
+        def flood_fill_step(curr_cluster, v_idx):
+            # return curr_cluster + jnp.matmul(curr_cluster, all_link_coinflips), None # note: only have to add because we don't add the identity to all_link_coinflips
+            return jnp.matmul(curr_cluster, all_link_coinflips), None
+        cluster, _ = jax.lax.scan(flood_fill_step, seed_row, jnp.arange(n))
+        cluster = jnp.minimum(cluster, 1)
+
+        cluster_move = jnp.multiply(move, jnp.expand_dims(cluster, axis=1)) # Particles not in the cluster are masked
+        mu_updated = rigid_body.RigidBody(mu.center + cluster_move, mu.orientation)
+
+        # check rejection with C * F * (1 - C)
+        # is_frustrated = jnp.matmul(jnp.matmul(cluster, frustrated), 1 - cluster)
+        is_frustrated = jnp.outer(cluster, 1-cluster) * frustrated
+        is_frustrated = is_frustrated.sum()
+
+        # note: terms in `jnp.where` have to be arrays
+        mu_center = jnp.where(is_frustrated, mu.center, mu_updated.center)
+        mu_orientation = jnp.where(is_frustrated, mu.orientation.vec, mu_updated.orientation.vec)
+        mu = rigid_body.RigidBody(center=mu_center,
+                                  orientation=rigid_body.Quaternion(mu_orientation))
+        traj.append(mu)
+    return mu, traj
+
+"""
+To compute for probabilities, we need the following:
+- proability of type of move (translational vs. orientation) -- 0.5
+  - note that in the same way we don't need the probability of the seed vertex, we may not need this
+- (i) probability of links in cluster
+  - just mask out rows/cols not in the cluster and take the product
+  - note: since we really care about log probs, can just take the differences in the boltzmann (with the appropriate min/max)
+  - also, have to avoid double counting (just divide by 2)
+- (ii) probability of no frustrated links on the boundary
+  - recall that `jnp.outer(cluster, 1-cluster)` has entry ij=1 when i is in the cluster and j is not
+  - note: probability of not being frustrated is the probability that all boundary links outright failed
+  - element-wise multiplication of `jnp.outer(cluster, 1-cluster)` and (1-A), where A is the probability of the prelinks forming
+    - note that we can optimize for summing over logs instead of multiplying probailities and then taking the log (by using the diffs that we eventually boltzmann)
+- (iii) Probability of 1 or more frustrated links
+  - 1 - (ii)
+- probability of step:
+  - if move accepted: (i) * (ii)
+  - if move not accepted: (i) * (iii)
+
+- note: would be nice if we could directly compute the rejection probability, akin to how monte carlo algorithms are tuned to have a particular acceptance rate
+- note: could also include a flag just to do single particle
+- note: maybe think of some simple test cases
+- note: look into references with megan on how and why cluster size is limited to test dynamics
+  - note: jamie makes a good point that large clusters will be more subject to being frustrated
+
+"""
+
+
+if __name__ == "__main__":
+    pass
+
+
+    # note: we still haven't quite worked out how to do max cluster size
+    # option 1: only do d iterations. problem is that graph size doesn't correspond to space
+    # option 2: do the whole thing then only sample d things.
+    # problems with all of these: maintaining connectedness, uniformly sampling something of size `d`
+    # Also note: are we only cecking furstareted for the boundary? does it matter?
