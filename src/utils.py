Correct 2d rotation??

src/utils.py
@@ -47,7 +47,6 @@ def gen_random_displacement(r_min, r_max, key):
     return jnp.array([x, y, z])
 
 
-
 def gen_random_displacement_2d(r_min, r_max, key):
     radii_key, theta_key = random.split(key, 2)
 
@@ -58,6 +57,30 @@ def gen_random_displacement_2d(r_min, r_max, key):
     y = radius * jnp.sin(theta)
 
     return jnp.array([x, y])
+
+
+def rand_2d_translation(mu, r_min, r_max, key):
+    trans_move = gen_random_displacement_2d(r_min, r_max, key)
+    nu = rigid_body.RigidBody(mu.center + trans_move, mu.orientation)
+    return nu
+
+def rand_2d_rotation(mu, a_max, theta_max, key):
+    n = mu.center.shape[0]
+    a_key, theta_key, center_key, u_key = random.split(key, 4)
+
+    center_idx = random.randint(center_key, shape=(), minval=0, maxval=n-1)
+    u_theta = random.uniform(u_key, minval=0, maxval=2*jnp.pi)
+    u = jnp.array([jnp.cos(u_theta), jnp.sin(u_theta)])
+    a = random.uniform(a_key, minval=0, maxval=a_max)
+    rot_center = mu[center_idx].center + a*u
+
+    theta = random.uniform(theta_key, minval=-theta_max, maxval=theta_max)
+    rot_matrix = jnp.array([[jnp.cos(theta), -jnp.sin(theta)], [jnp.sin(theta), jnp.cos(theta)]])
+
+    nu_center = (rot_matrix @ (mu.center - rot_center).T).T + rot_center
+    nu = rigid_body.RigidBody(nu_center, mu.orientation + theta)
+    return nu
+
 
 
 def get_rand_rigid_body(n, box_size, key):

src/vmmc_2d.py
@@ -15,7 +15,8 @@ import utils
 
 
 
-def vmmc(body, gen_tables_fn, key, n_steps=10, temp=0.3, rot_threshold=0.5):
+def vmmc(body, gen_tables_fn, key, n_steps=10, temp=0.3, rot_threshold=0.5,
+         r_min=0.0, r_max=1.0, a_max = 0.5, theta_max=jnp.pi/6):
     n = body.center.shape[0]
     seed_vertices_key, move_key, key = random.split(key, 3)
     seed_vertices = jax.random.randint(seed_vertices_key, (n_steps,), minval=0, maxval=n-1)
@@ -39,14 +40,28 @@ def vmmc(body, gen_tables_fn, key, n_steps=10, temp=0.3, rot_threshold=0.5):
     def step_fn(mu, iter_key, seed_vertex, move_type):
         iter_key, move_key = random.split(iter_key, 2)
 
+        """
         trans_move = jnp.where(move_type == 0,
-                               utils.gen_random_displacement_2d(r_min=0.5, r_max=1.0, key=move_key),
+                               utils.gen_random_displacement_2d(r_min=r_min, r_max=r_max, key=move_key),
                                identity_translation)
+
         rot_move = jnp.where(move_type == 1,
                              random.uniform(move_key, minval=0.0, maxval=jnp.pi / 6),
                              identity_theta)
 
         nu = rigid_body.RigidBody(mu.center + trans_move, mu.orientation + rot_move)
+        """
+        nu_rot = utils.rand_2d_rotation(mu, a_max, theta_max, move_key)
+        nu_trans = utils.rand_2d_translation(mu, r_min, r_max, move_key)
+
+        nu_center = jnp.where(move_type == 0,
+                              nu_trans.center,
+                              nu_rot.center)
+        nu_orientation = jnp.where(move_type == 0,
+                                   nu_trans.orientation,
+                                   nu_rot.orientation)
+        nu = rigid_body.RigidBody(nu_center, nu_orientation)
+
 
         eps_mu_mu, eps_mu_nu, eps_nu_mu = gen_tables_fn(mu, nu)
 